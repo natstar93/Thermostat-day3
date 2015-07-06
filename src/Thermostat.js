1259c1c6dfe495bbc24fdb3079a6e7f4a4debf62
Simon Legg <leggsimon@gmail.com>
2015-07-06 17:16:09 +0100

    Third test passing & Fourth test written

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -5,3 +5,7 @@ var Thermostat = function() {
 Thermostat.prototype.increase = function () {
   (this.temperature = this.temperature + 1)
 };
+
+Thermostat.prototype.decrease = function () {
+  this.temperature = this.temperature - 1;
+};
