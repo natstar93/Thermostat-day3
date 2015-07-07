44a9ef5f351fe97ad1f6f679c26134ba40155d19
mohamed Issaq <minhajraz10@yahoo.com>
2015-07-07 18:01:45 +0100

    added some functions on the controller

--- src/thermoController.js
+++ src/thermoController.js
@@ -1,4 +1,11 @@
 var thermostat = new Thermostat;
 
 document.getElementById("temperature").innerHTML= (thermostat.temperature);
-document.getElementById("up").onClick = thermostat.increase();
+// document.getElementById("up").onClick = thermostat.increase();
+function showtemp() {
+  return thermostat.temperature;
+};
+function increase() {
+  document.getElementById("up") = thermostat.increase();
+  return showtemp();
+};
