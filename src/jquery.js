e3d2b37f0e6e93bc342c9ed3200f09cbb87659cc
mohamed Issaq <minhajraz10@yahoo.com>
2015-07-08 15:36:29 +0100

    added weather api into the jquery.js file

--- src/jquery.js
+++ src/jquery.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
 $('#temperature').hover(function() {
 $(this).css('font-size', '100px')
 });
-  
+
+  $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric", success: function(result){
+    $("#currentTemp").html(result.main.temp + '°C');
+        }
+    });
+
 });
