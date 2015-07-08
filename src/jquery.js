ae6a3f5e4ea5efc06b31df8e904e3060ae3d9c2f
Natalie Akam <natalie.akam@gmail.com>
2015-07-08 16:43:25 +0100

    Added city names

--- src/jquery.js
+++ src/jquery.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
+  
   $('#temperature').hover(function() {
     $(this).css('font-size', '100px')
   });
-
-  // $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric", success: function(result){
-  //   $("#currentTemp").html(result.main.temp + '°C');
-  // }
-  // });
   
-  // $.ajax({url: "http://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743&units=metric", success: function(result){
-  //   $("#currentTemp").html(result.list[0].main.temp + '°C');
-  // }
-  // });
+  $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric", success: function(result){
+            $("#currentTemp").html(result.main.temp + '°C');
+            $("#city").html(result.name);
+        }});
+ 
 
   $('#cities').change(function(){
   	// alert(this.value);
@@ -19,13 +16,11 @@ $(document).ready(function() {
   	var link = 'http://api.openweathermap.org/data/2.5/find?q=' + this.value + '&units=metric';
   	// Extract the temperature and name from the data
   	// Change the HTML of the currentTemp element
-  	 
-
-  $.ajax({url: link, success: function(result){
-  	 alert(link);
-    $("#currentTemp").html(result.list[0].main.temp + '°C');
-  }
-  });
+    $.ajax({url: link, success: function(result){
+    $("#city").html(result.list[0].name);
+      $("#currentTemp").html(result.list[0].main.temp + '°C');
+    }
+    });
 
   });
 
