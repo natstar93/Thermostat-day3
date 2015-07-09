9c73e49eafc34316c81970bf117e9d3346bb9124
Natalie Akam <natalie.akam@gmail.com>
2015-07-09 12:41:51 +0100

    Added feature to display temp at current location

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   
   var thermostat = new Thermostat;
   showTemperature();
+  navigator.geolocation.getCurrentPosition(onPositionUpdate);
 
   $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric", success: function(result){
     $("#currentTemp").html(result.main.temp + '°C');
@@ -16,7 +17,21 @@ $(document).ready(function() {
       $("#currentTemp").html(result.list[0].main.temp + '°C');
     }});
   });
-
+  
+  function onPositionUpdate(position) {
+   
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    $.ajax({
+        url : "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            var location = parsed_json['name'];
+            var temp_c = parsed_json['main']['temp'];
+            $('#local_temp').html("Current temperature in " + location + " is: " + temp_c + "&deg;C");
+        }
+    });
+  }
 
   function showTemperature() { 
     $("#temperature").html(thermostat.temperature)
