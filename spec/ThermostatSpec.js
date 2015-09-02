4cbb051000e92424315e77321ab91918af953637
Natalie Akam <natalie.akam@gmail.com>
2015-09-02 18:24:01 +0100

    Refactored tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,40 +1,36 @@
 describe('Thermostat', function() {
-  
-  var thermostat
-  
+
+  var thermostat;
+
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  describe('temperature defaults', function() {
-    it('to 20ºC', function() {
+  describe('when initialised', function() {
+    it('starts at 20ºC', function() {
       expect(thermostat.temperature).toEqual(20);
     });
   });
 
-  describe('increases', function() {
-    it('the temperature by 1ºC', function() {
+  describe('temperature', function() {
+    it('can be increased by 1ºC', function() {
       thermostat.increase();
       expect(thermostat.temperature).toEqual(21);
     });
-  });
 
-  describe('decreases', function() {
-    it('the temperature by 1ºC', function() {
+    it('can be decreased by 1ºC', function() {
       thermostat.decrease();
       expect(thermostat.temperature).toEqual(19);
     });
-  });
 
-  describe('cannot decrease if', function() {
-    it('temperature is <= 10ºC', function() {
+    it('cannot be decreased if temperature is 10ºC', function() {
       thermostat.temperature = 10;
       thermostat.decrease();
       expect(thermostat.temperature).toEqual(10);
     });
   });
 
-  describe('power saving mode,', function() {
+  describe('power saving mode', function() {
     it('defaults to on', function() {
       expect(thermostat.powerSave).toEqual(true);
     });
@@ -50,22 +46,20 @@ describe('Thermostat', function() {
       expect(thermostat.powerSave).toEqual(true);
     });
 
-    it('changes temp to 25 if above 25 when turned on', function() {
-      thermostat.temperature = 26;
+    it('changes temperature to 25ºC if above 25ºC', function() {
+      thermostat.temperature = 27;
       thermostat.powerSave = false;
       thermostat.powerSaveToggle();
       expect(thermostat.temperature).toEqual(25);
     });
-  });
 
-  describe('when power save is', function() {
-    it('on, the max temperature is 25ºC', function() {
+    it('when on, max temperature is 25ºC', function() {
       thermostat.temperature = 25;
       thermostat.increase();
       expect(thermostat.temperature).toEqual(25);
     });
 
-    it('off, the max temperature is 32ºC', function() {
+    it('when off, max temperature is 32ºC', function() {
       thermostat.powerSave = false;
       thermostat.temperature = 32;
       thermostat.increase();
@@ -73,32 +67,27 @@ describe('Thermostat', function() {
     });
   });
 
-  describe('reset the temperature', function() {
-    it('changes the temperature to 20', function() {
+  describe('resetting temperature', function() {
+    it('changes temperature to 20ºC', function() {
       thermostat.temperature = 22;
       thermostat.resetTemperature();
       expect(thermostat.temperature).toEqual(20);
     });
   });
 
-  describe('mode', function() {
-    it('outputs cool', function() {
+  describe('temperature mode', function() {
+    it('is cool if temperature is 15ºC', function() {
       thermostat.temperature = 15;
       expect(thermostat.mode()).toEqual('cool');
     });
-  });
 
-  describe('mode', function() {
-    it('outputs warm', function() {
+    it('is warm if temperature is 20ºC', function() {
       expect(thermostat.mode()).toEqual('warm');
     });
-  });
 
-  describe('mode', function() {
-    it('outputs hot', function() {
+    it('is hot if temperature is 25ºC', function() {
       thermostat.temperature = 27;
       expect(thermostat.mode()).toEqual('hot');
     });
   });
 });
-
